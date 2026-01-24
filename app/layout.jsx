612b2d28f2d953d2d63b502d416e5ae2c11c8ba9
ilyas lokhand <ilokhandwala786@gmail.com>
2026-01-24 10:57:28 +0530

    first commit

---
+++ app/layout.jsx
@@ -0,0 +1,18 @@
+import "@/assets/styles/global.css"
+import { Poppins } from "next/font/google"
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  display: "swap",
+})
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={poppins.className}>
+        {children}
+      </body>
+    </html>
+  )
+}
---
+++ app/page.jsx
@@ -0,0 +1,9 @@
+const Homepage = () => {
+  return (
+    <div className="bg-red-600">
+      hello
+    </div>
+  )
+}
+
+export default Homepage
