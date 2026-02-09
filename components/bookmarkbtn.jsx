293e34208dd3d91373a9ed90666af5fe79928cd0
ilyas lokhand <ilokhandwala786@gmail.com>
2026-02-09 14:15:20 +0530

    refactor: clean up code formatting and improve readability in BookMarkButton component

--- components/bookmarkbtn.jsx
+++ components/bookmarkbtn.jsx
@@ -11,15 +11,13 @@ const BookMarkButton = ({ propertyId }) => {
   const [loading, setLoading] = useState(false);
   const { data: session } = useSession();
 
-
-
   const handleclick = async () => {
     if (!session) {
       toast.error("You must be logged in to bookmark a property");
     } else {
       try {
         const result = await BookMark(propertyId);
-        setIsBookmarked((prev)=> !prev)
+        setIsBookmarked((prev) => !prev);
         toast.success(result.message);
       } catch (error) {
         toast.error(error.message || "Something went wrong");
@@ -27,25 +25,25 @@ const BookMarkButton = ({ propertyId }) => {
     }
   };
 
-return (
-  <>
-    {isBookmarked ? (
-      <button
-        className="bg-red-500 hover:bg-red-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center"
-        onClick={handleclick}
-      >
-        <FaBookmark className="mr-2" /> Remove Bookmark
-      </button>
-    ) : (
-      <button
-        className="bg-blue-500 hover:bg-blue-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center"
-        onClick={handleclick}
-      >
-        <FaBookmark className="mr-2" /> Bookmark Property
-      </button>
-    )}
-  </>
-);
-}
+  return (
+    <>
+      {isBookmarked ? (
+        <button
+          className="bg-red-500 hover:bg-red-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center"
+          onClick={handleclick}
+        >
+          <FaBookmark className="mr-2" /> Remove Bookmark
+        </button>
+      ) : (
+        <button
+          className="bg-blue-500 hover:bg-blue-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center"
+          onClick={handleclick}
+        >
+          <FaBookmark className="mr-2" /> Bookmark Property
+        </button>
+      )}
+    </>
+  );
+};
 
 export default BookMarkButton;
